525fae2126879a5f845330b8bc12086ef832d963
Giuseppe Antonio Diviggiano <giuseppe.diviggiano@linksmt.it>
2024-04-13 15:37:19 +0200

    first challenge - done

--- othersBalance.ts
+++ othersBalance.ts
@@ -1,18 +1,18 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
-const suppliedPublicKey = process.argv[2];
-if (!suppliedPublicKey) {
-  throw new Error("Provide a public key to check the balance of!");
+const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed");
+const addressTolySol = "GgJJRwLg9NzFQ97o1CJLGLp1KLSUMBwFc6eQNVEr4fbW1";
+
+try {
+  const publicKey = new PublicKey(addressTolySol);
+  const balanceInLamports = await connection.getBalance(publicKey);
+  const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+  if (balanceInLamports === 0) {
+    console.log(`❌ L'address ${publicKey} non ha SOL, potrebbe non essere in uso.`);
+  } else {
+    console.log(`✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL} SOL!`);
+  }
+} catch (error) {
+  console.log(`❌ Errore: ${error.message}`);
 }
-
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
-
-const publicKey = new PublicKey(suppliedPublicKey);
-
-const balanceInLamports = await connection.getBalance(publicKey);
-
-const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
-
-console.log(
-  `✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
-);
