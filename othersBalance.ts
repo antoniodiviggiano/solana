d4b1803acd21a39d6dbed507a3d89172ec138797
Giuseppe Antonio Diviggiano <giuseppe.diviggiano@linksmt.it>
2024-04-13 13:10:43 +0200

    otherBalance da completare - esercizio

--- othersBalance.ts
+++ othersBalance.ts
@@ -1,22 +1,29 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js";
 
 let suppliedPublicKey = process.argv[2];
-if (!suppliedPublicKey) {
-  //throw new Error("Provide a public key to check the balance of!");
-  const connectionMain = new Connection(clusterApiUrl("mainnet-beta")).tol;
+
+console.log("Supplied public key:", suppliedPublicKey); // Debugging log
+
+if (!suppliedPublicKey || suppliedPublicKey.trim() === "") {
+  const tolySolana = "toly.sol"
+  const connectionMain = new Connection("https://api.mainnet-beta.solana.com", "confirmed")
+  const publicKeyMain = new PublicKey(tolySolana);
+  const balanceInLamports = await connectionMain.getBalance(publicKeyMain);
+  const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+  console.log(
+    `✅ Finished! The balance for the wallet ${tolySolana} at address ${publicKeyMain} is ${balanceInSOL}!`
+  );
+
+} else {
+  const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+  const publicKey = new PublicKey(suppliedPublicKey);
+  const balanceInLamports = await connection.getBalance(publicKey);
+  const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+  console.log(
+    `✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
+  );
 }
 
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
-
-const publicKey = new PublicKey(suppliedPublicKey);
-
-const balanceInLamports = await connection.getBalance(publicKey);
-
-const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
-
-console.log(
-  `✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
-);
 
 // % npx esrun check-balance.ts (some wallet address)
 //✅ Finished! The balance for the wallet at address 31ZdXAvhRQyzLC2L97PC6Lnf2yWgHhQUKKYoUo9MLQF5 is 3!
