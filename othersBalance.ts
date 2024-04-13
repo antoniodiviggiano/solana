bea02dd983f85d052b2dcbb7367bbb33ced6e8e7
Giuseppe Antonio Diviggiano <giuseppe.diviggiano@linksmt.it>
2024-04-13 14:51:20 +0200

    revert otherBalance

--- othersBalance.ts
+++ othersBalance.ts
@@ -1,30 +1,18 @@
-import { Connection, LAMPORTS_PER_SOL, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
-let suppliedPublicKey = process.argv[2];
-
-console.log("Supplied public key:", suppliedPublicKey); // Debugging log
-
-if (!suppliedPublicKey || suppliedPublicKey.trim() === "") {
-  const tolySolana = "toly.sol"
-  const connectionMain = new Connection("https://api.mainnet-beta.solana.com", "confirmed")
-  const publicKeyMain = new PublicKey(tolySolana);
-  const balanceInLamports = await connectionMain.getBalance(publicKeyMain);
-  const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
-  console.log(
-    `✅ Finished! The balance for the wallet ${tolySolana} at address ${publicKeyMain} is ${balanceInSOL}!`
-  );
-
-} else {
-  const connection = new Connection("https://api.devnet.solana.com", "confirmed");
-  const publicKey = new PublicKey(suppliedPublicKey);
-  const balanceInLamports = await connection.getBalance(publicKey);
-  const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
-  console.log(
-    `✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
-  );
+const suppliedPublicKey = process.argv[2];
+if (!suppliedPublicKey) {
+  throw new Error("Provide a public key to check the balance of!");
 }
 
+const connection = new Connection("https://api.devnet.solana.com", "confirmed");
 
-// % npx esrun check-balance.ts (some wallet address)
-//✅ Finished! The balance for the wallet at address 31ZdXAvhRQyzLC2L97PC6Lnf2yWgHhQUKKYoUo9MLQF5 is 3!
-// all this in othersBalance
+const publicKey = new PublicKey(suppliedPublicKey);
+
+const balanceInLamports = await connection.getBalance(publicKey);
+
+const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
+
+console.log(
+  `✅ Finished! The balance for the wallet at address ${publicKey} is ${balanceInSOL}!`
+);
